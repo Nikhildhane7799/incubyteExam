bcc647be34cbcf7317f473d441d866cd4923cf5a
NikhilDhane <nikhildhane1@gmail.com>
2021-10-17 23:27:26 +0530

    Test1

---
+++ StringCalculator/src/calculator/StringCalculatorTest.java
@@ -0,0 +1,18 @@
+package calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringCalculatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testEmptyString() {
+		assertEquals(0, Calculator.add(""));
+	}
+
+}
